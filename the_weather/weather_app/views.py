a4c2276f656212f3098bcd537c871b92706cec0b
Sky <dungchogithub@gmail.com>
2018-10-07 00:15:06 -0700

    test the api for the weather in Las Vegas

--- the_weather/weather_app/views.py
+++ the_weather/weather_app/views.py
@@ -1,5 +1,13 @@
+import requests
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+	url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=98bcd33249fa8d189f0a429fe073bd37'
+	city = 'Las Vegas'
+
+	r =	requests.get(url.format(city))
+	data = r.json()
+	print(r)
+	print(data)
 	return render(request, 'weather/weather.html')
