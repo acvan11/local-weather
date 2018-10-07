49b81bbaf889b36d57641e5793febb98d25b8c62
Sky <dungchogithub@gmail.com>
2018-10-07 00:50:01 -0700

    create 3 cities in database and check the weather in server

--- the_weather/weather_app/views.py
+++ the_weather/weather_app/views.py
@@ -1,19 +1,29 @@
 import requests
 from django.shortcuts import render
+from .models import City
 
 # Create your views here.
 def index(request):
 	url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=98bcd33249fa8d189f0a429fe073bd37'
 	city = 'Las Vegas'
 
-	r =	requests.get(url.format(city)).json()
+	cities = City.objects.all()
 
-	city_weather = {
-		'city': city,
-		'temperature' : r['main']['temp'],
-		'description' : r['weather'][0]['description'],
-		'icon' :r['weather'][0]['icon'],
-	}
+	weather_data = []
+
+	for city in cities:		
+		r =	requests.get(url.format(city)).json()
+
+		city_weather = {
+			'city': city,
+			'temperature' : r['main']['temp'],
+			'description' : r['weather'][0]['description'],
+			'icon' :r['weather'][0]['icon'],
+		}
+
+		weather_data.append(city_weather)
+
+	print(weather_data)
 	
 	context = {'city_weather': city_weather}
 	return render(request, 'weather/weather.html', context)
