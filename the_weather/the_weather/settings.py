7ecd206f63460d9387b15f4dcfd4674ddde09765
Sky <dungchogithub@gmail.com>
2018-10-06 22:16:35 -0700

    edit setting and migrate the database

--- the_weather/the_weather/settings.py
+++ the_weather/the_weather/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'weather_app',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,9 @@ WSGI_APPLICATION = 'the_weather.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'local_weather',
+        'HOST': 'localhost',
     }
 }
 
