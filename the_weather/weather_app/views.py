ecbad3419467ad85f315800b16874f2b2297c6e9
Sky <dungchogithub@gmail.com>
2018-10-07 00:31:01 -0700

    print on the server the weather in Las Vegas

--- the_weather/weather_app/views.py
+++ the_weather/weather_app/views.py
@@ -6,8 +6,14 @@ def index(request):
 	url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=98bcd33249fa8d189f0a429fe073bd37'
 	city = 'Las Vegas'
 
-	r =	requests.get(url.format(city))
-	data = r.json()
-	print(r)
-	print(data)
+	r =	requests.get(url.format(city)).json()
+
+
+	city_weather = {
+		'city': city,
+		'temperature' : r['main']['temp'],
+		'description' : r['weather'][0]['description'],
+		'icon' :r['weather'][0]['icon'],
+	}
+	print(city_weather)
 	return render(request, 'weather/weather.html')
